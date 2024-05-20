7972bf6b162e13602b0aa244ebb557bc83a56c57
NxtEcho <2224316683@qq.com>
2024-05-20 22:43:08 +0800

    update 22:43

---
+++ seam/seam.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <opencv2/opencv.hpp>
+
+class Seam {
+    void getEnergy();
+    void getVertical();
+    void getHorizontal();
+};
+
