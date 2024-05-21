2011d87501a60139ff0db639f6aa1f3f2a44f36e
NxtEcho <2224316683@qq.com>
2024-05-21 19:03:12 +0800

    update 19:03

--- seam/seam.cpp
+++ seam/seam.cpp
@@ -20,6 +20,9 @@ void Seam::getVertical(const Mat &img) {
     Mat M, from;
     M.create(img.size(), CV_64FC1);
     from.create(img.size(), CV_32SC1);
+    for (int j = 0; j < img.rows; j++) {
+        M.at<double>(0, j) = E.at<double>(0, j);
+    }
     for (int i = 1; i < img.rows; i++) {
         for (int j = 0; j < img.cols; j++) {
             M.at<double>(i, j) = M.at<double>(i - 1, j);
@@ -38,4 +41,25 @@ void Seam::getVertical(const Mat &img) {
 }
 
 void Seam::getHorizontal(const Mat &img) {
+    Mat M, from;
+    M.create(img.size(), CV_64FC1);
+    from.create(img.size(), CV_32SC1);
+    for (int i = 0; i < img.rows; i++) {
+        M.at<double>(i, 0) = E.at<double>(i, 0);
+    }
+    for (int j = 1; j < img.cols; j++) {
+        for (int i = 0; i < img.rows; i++) {
+            M.at<double>(i, j) = M.at<double>(i, j - 1);
+            from.at<double>(i, j) = i;
+            if (i > 0 && M.at<double>(i - 1, j - 1) < M.at<double>(i, j)) {
+                M.at<double>(i, j) = M.at<double>(i - 1, j - 1);
+                from.at<double>(i, j) = i - 1;
+            }
+            if (i < img.rows - 1 && M.at<double>(i + 1, j - 1) < M.at<double>(i, j)) {
+                M.at<double>(i, j) = M.at<double>(i + 1, j - 1);
+                from.at<double>(i, j) = i + 1;
+            }
+            M.at<double>(i, j) += E.at<double>(i, j);
+        }
+    }
 }
