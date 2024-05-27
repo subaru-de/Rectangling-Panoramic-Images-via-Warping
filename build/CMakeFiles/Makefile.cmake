463164302935aded935be887c18866a2ded0583e
NxtEcho <2224316683@qq.com>
2024-05-28 01:39:33 +0800

    update 1:39

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -40,6 +40,9 @@ set(CMAKE_MAKEFILE_DEPENDS
   "/usr/share/cmake-3.22/Modules/Platform/Linux-GNU.cmake"
   "/usr/share/cmake-3.22/Modules/Platform/Linux.cmake"
   "/usr/share/cmake-3.22/Modules/Platform/UnixPaths.cmake"
+  "/usr/share/eigen3/cmake/Eigen3Config.cmake"
+  "/usr/share/eigen3/cmake/Eigen3ConfigVersion.cmake"
+  "/usr/share/eigen3/cmake/Eigen3Targets.cmake"
   )
 
 # The corresponding makefile is:
--- include/Energy.h
+++ include/Energy.h
@@ -4,10 +4,12 @@
 #include <GLFW/glfw3.h>
 #include <opencv2/opencv.hpp>
 #include <Rectangling.h>
+#include <Eigen/Core>
 
 using std::cout;
 using std::string;
 using namespace cv;
+using namespace Eigen;
 
 class Energy {
 private:
@@ -15,11 +17,15 @@ private:
     const double lambdaBound = 1e8;
     Mat &img;
     vecvecP &ver, &nver;
+    MatrixXd A(0, 4), B, C, V(0, 1), Y;
 public:
     Energy(Mat &img, vecvecP &ver, vecvecP &nver);
     double getEnergy();
+    // 为了减少一次遍历，在 shapeTerm 函数中同时执行 getV 与 getA
     double shapeTerm();
+    // 在 lineTerm 中执行 getC
     double lineTerm();
+    // 在 boundTerm 中执行 getB
     double boundTerm();
 };
 
@@ -33,38 +39,58 @@ double Energy::getEnergy() {
 }
 
 double Energy::shapeTerm() {
-    Mat A, AT, ATA, invATA, V, I;
-    I = Mat::eye(8, 8, CV_64F);
+    // Mat A, AT, ATA, invATA, V, I;
+    // I = Mat::eye(8, 8, CV_64F);
     double E = 0, cnt = 0;
     for (int i = 0; i < ver.size(); i++) {
         for (int j = 0; j < ver[i].size(); j++) {
             if (!i || !j) continue;
-            A = (
-                Mat_<double>(8, 4) <<
+            // A = (
+            //     Mat_<double>(8, 4) <<
+            //     ver[i - 1][j - 1].x, -ver[i - 1][j - 1].y, 1, 0,
+            //     ver[i - 1][j - 1].y, ver[i - 1][j - 1].x, 0, 1,
+                
+            //     ver[i - 1][j].x, -ver[i - 1][j].y, 1, 0,
+            //     ver[i - 1][j].y, ver[i - 1][j].x, 0, 1,
+                
+            //     ver[i][j - 1].x, -ver[i][j - 1].y, 1, 0,
+            //     ver[i][j - 1].y, ver[i][j - 1].x, 0, 1,
+                
+            //     ver[i][j].x, -ver[i][j].y, 1, 0,
+            //     ver[i][j].y, ver[i][j].x, 0, 1
+            // );
+            // V = (
+            //     Mat_<double>(8, 1) <<
+            //     nver[i - 1][j - 1].x, nver[i - 1][j - 1].y,
+            //     nver[i - 1][j].x, nver[i - 1][j].y,
+            //     nver[i][j - 1].x, nver[i][j - 1].y,
+            //     nver[i][j].x, nver[i][j].y
+            // );
+            // transpose(A, AT);
+            // ATA = AT * A;
+            // invert(ATA, invATA, DECOMP_LU);
+            MatrixXd Aq(8, 4);
+            Aq <<
                 ver[i - 1][j - 1].x, -ver[i - 1][j - 1].y, 1, 0,
                 ver[i - 1][j - 1].y, ver[i - 1][j - 1].x, 0, 1,
-                
                 ver[i - 1][j].x, -ver[i - 1][j].y, 1, 0,
                 ver[i - 1][j].y, ver[i - 1][j].x, 0, 1,
-                
                 ver[i][j - 1].x, -ver[i][j - 1].y, 1, 0,
                 ver[i][j - 1].y, ver[i][j - 1].x, 0, 1,
-                
                 ver[i][j].x, -ver[i][j].y, 1, 0,
-                ver[i][j].y, ver[i][j].x, 0, 1
-            );
-            V = (
-                Mat_<double>(8, 1) <<
+                ver[i][j].y, ver[i][j].x, 0, 1;
+            Vq <<
                 nver[i - 1][j - 1].x, nver[i - 1][j - 1].y,
                 nver[i - 1][j].x, nver[i - 1][j].y,
                 nver[i][j - 1].x, nver[i][j - 1].y,
-                nver[i][j].x, nver[i][j].y
-            );
-            transpose(A, AT);
-            ATA = AT * A;
-            invert(ATA, invATA, DECOMP_LU);
-
-            Mat tmp = (A * invATA * AT - I) * V;
+                nver[i][j].x, nver[i][j].y;
+            
+            Aq = (Aq * (Aq.transpose * Aq).inverse() * Aq.transpose() - MatrixXd::Identity(8, 8));
+            A.resize(A.rows() + Aq.rows(), A.cols());
+            A << Aq;
+            V.resize(V.rows() + Vq.rows(), V.cols());
+            V << Vq;
+            VectorXd tmp = Aq * Vq;
             E += tmp.dot(tmp);
             cnt += 1.0;
         }
